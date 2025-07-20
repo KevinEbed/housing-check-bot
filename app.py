73c9ba8f703e42b80c52e4ff15505e5746de026f
KevinEbed <133675857+KevinEbed@users.noreply.github.com>
2025-07-20 13:25:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def stop_monitoring(url_id):
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
